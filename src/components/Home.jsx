f7185beeb26dc1984fc1a3ef8477ea50d10e09c2
riley <16313469+karateking007@users.noreply.github.com>
2021-07-29 21:18:05 -0500

    Updated home page and app.css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,24 +4,18 @@ import { Link } from "react-router-dom";
 
 function Home() {
     return (
-        <div id="home-container" className="container">
-            <div className="row home-row align-items-center h-100">
-                <div className="hero-text col-lg-5">
-                    <h1 className="headline font-weight-bold">Hi, <br />I'm Riley!</h1>
-                    <p className="headline-sub-text mt-4">A web developer.</p>
-                    <Link to="/work"><button id="home-button" className="btn btn-blue mt-4">
-                        See My Work
-                    </button></Link>
-                </div>
-                <div className="col-lg-7 col-graphic d-flex justify-content-end align-items-center h-100">
-                    <img
-                        className="graphic img-fluid"
-                        src={graphic}
-                        alt="..."
-                    />
+        <section className="test text-light p-sm-5 p-5 mt-lg-5 text-center text-lg-start">
+            <div className="container">
+                <div className="d-lg-flex align-items-center justify-content-between">
+                    <div>
+                        <h1 className="display-1 fw-bold">Hi,<br />I'm Riley</h1>
+                        <p className="lead my-4 fw-bold">A web developer.</p>
+                        <button className="btn btn-blue">View My Work</button>
+                    </div>
+                    <img className="img-fluid w-100 my-5 my-lg-0 mx-auto mx-lg-0 px-2" id="home-graphic" src={graphic} alt="Man using computer." />
                 </div>
             </div>
-        </div>
+        </section>
     );
 }
 
