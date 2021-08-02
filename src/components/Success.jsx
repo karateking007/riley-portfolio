47460846b94c32113e0ea1284d4876f7492588af
riley <16313469+karateking007@users.noreply.github.com>
2021-08-01 20:25:37 -0500

    Added Success component

---
+++ src/components/Success.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Contact() {
+    return (
+        <section className="d-flex align-items-center p-0 p-md-5 py-5">
+            <div className="container">
+                <h2 className="text-center text-light text-uppercase">Thank you</h2>
+            </div>
+        </section>
+    );
+}
+
+export default Contact;
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Footer } from "./Footer";
 export { default as Home } from "./Home";
 export { default as Work } from "./Work";
 export { default as Contact } from "./Contact";
+export { default as Success } from "./Success";
