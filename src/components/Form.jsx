5603718baf1a88000c8aae5e0173ee6eb83745f9
riley <16313469+karateking007@users.noreply.github.com>
2021-08-02 20:11:28 -0500

    Updated Form page

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,37 +1,58 @@
-import React, { useState, useEffect } from 'react';
-function Form() {
-    const [success, setSuccess] = useState(false);
+import React from 'react';
 
-    useEffect(() => {
-        if (window.location.search.includes('success=true')) {
-            setSuccess(true);
-        }
-    }, []);
+const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+}
 
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { name: "", email: "", message: "" };
+    }
 
-    return (
-        <form name="contact" method="post" action="/form/?success=true">
-            {success && (
-                <p style={{ color: "green" }}>Thanks for your message! </p>
-            )}
-            <input type="hidden" name="form-name" value="contact" />
-            <p>
-                <label htmlFor="name">Name</label> <br />
-                <input type="text" id="name" name="name" required />
-            </p>
-            <p>
-                <label htmlFor="email">Email</label> <br />
-                <input type="email" id="email" name="email" required />
-            </p>
-            <p>
-                <label htmlFor="message">Message</label> <br />
-                <textarea id="message" name="message" required></textarea>
-            </p>
-            <p>
-                <input type="submit" value="Submit message" />
-            </p>
-        </form>
-    );
+    /* Here’s the juicy bit for posting the form submission */
+
+    handleSubmit = e => {
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact", ...this.state })
+        })
+            .then(() => alert("Success!"))
+            .catch(error => alert(error));
+
+        e.preventDefault();
+    };
+
+    handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+    render() {
+        const { name, email, message } = this.state;
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <p>
+                    <label>
+                        Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        Message: <textarea name="message" value={message} onChange={this.handleChange} />
+                    </label>
+                </p>
+                <p>
+                    <button type="submit">Send</button>
+                </p>
+            </form>
+        );
+    }
 }
 
 export default Form;
