1be6a213036e97e82a68a00afa2cd911ba587955
riley <16313469+karateking007@users.noreply.github.com>
2021-08-04 21:06:17 -0500

    Updated Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
                     <div className="mb-3 mb-md-4 mb-lg-0">
                         <h1 className="display-1 fw-bold" id="lg-heading">Hi,<br />I'm Riley</h1>
                         <p className="lead my-4 fw-bold" id="lg-sub-heading">A web developer.</p>
-                        <Link to="/work" className="nav-link"><button className="btn btn-green">View My Work</button></Link>
+                        <Link to="/work"><button className="btn btn-green">View My Work</button></Link>
                     </div>
                     <img className="img-fluid w-100 mt-4 mt-lg-0 mx-auto mx-lg-0 px-2" id="home-graphic" src={graphic} alt="Man using computer." />
                 </div>
